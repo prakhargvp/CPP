5070c760aa2c2bc658546a554df4910c32bf1054
Prakhar Agrawal <prakhargvp@gmail.com>
2017-03-06 23:31:12 +0530

    Adding Comments
    About Static Member Variables

--- Constructor_Destructor/basic-flow1.cpp
+++ Constructor_Destructor/basic-flow1.cpp
@@ -1,30 +1,37 @@
+//Header Files 
 #include<iostream>
 using namespace std;
+// Sample Class
 class sample
 {
     int a;
-static int i;
+    //These static variables are stored on static storage area , not in stack.
+    static int i;
+// Access Modifier :  Public
 public:
-sample()
-{
- a = ++i;
- cout<<"calling the construtor :"<<a<<endl;
-}
-   ~ sample()
-   {
-    cout<<"calling of destructor :"<<a<<endl;
+    // Constructor Calling 
+    sample(){
+        a = ++i;
+        cout<<"calling the construtor :"<<a<<endl;
+    }
+    // Destructor Calling
+   ~sample(){
+        cout<<"calling of destructor :"<<a<<endl;
    }
 };
+// Static member variables (data members) are not initialied using constructor, 
+// because these are not dependent on object initialization.
+// Also, it must be initialized explicitly, always outside the class. If not initialized, Linker will give error.
 int sample::i;
+// Main Calling Function
 int main()
 {
-sample obj;
-{
- sample obj1;
- {
-  sample obj2;
- }
-}
+    sample obj;
+    {
+        sample obj1;
+        {
+            sample obj2;
+        }
+    }
 return 0;
 }
-
