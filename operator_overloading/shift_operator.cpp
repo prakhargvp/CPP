b5ed7ebd4a1c5d7b8b8396eba22b3d83d48d718a
prakhargvp <prakhargvp@gmail.com>
2017-05-16 10:59:01 +0530

    Adding shift operator overload

---
+++ operator_overloading/shift_operator.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class Test{
+	int a;
+public:
+	Test(){
+		a = 0;
+	}
+	Test(int n){
+		a = n;
+	}
+	// left-shift operator
+	// left-shift *= 2
+	Test operator << (int t){
+		a = a << t;	
+		return *this;
+	}
+	// Right-shift Operator
+	// right-shift /= 2
+	Test operator >> (int t){
+		a = a >> t;
+		return *this;
+	}
+	void print(){
+		cout << a << endl;
+	}
+};
+int main(){
+	Test obj1(1);
+	obj1.print();	// 1
+	// left shift
+	// obj1 = obj1 << 5
+	obj1 << 2;
+	obj1.print();	// 4	
+	// right shift
+	obj1 >> 1;
+	obj1.print();	// 2
+return 0;
+}
